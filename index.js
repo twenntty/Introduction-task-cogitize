54e2d097ff6996733b5def24061c7dd7c864abac
Mark Bilous <belousme601@gmail.com>
2024-03-08 00:02:33 +0200

    add npm mongodb

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ require('./helpers/init_mongodb')
 
 const app = express()
 const PORT = 3000
+
+app.use(express.json())
+
+async function startApp() {
+  try{
+    app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
+  }
+  catch{
+    console.log(e)
+  }
+}
+
+startApp();
