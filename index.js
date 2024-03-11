0c92368213618b67dc0d8a6f18a92a0e6c09cce4
Mark Bilous <belousme601@gmail.com>
2024-03-11 21:20:16 +0200

    minus error

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something went wrong!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
